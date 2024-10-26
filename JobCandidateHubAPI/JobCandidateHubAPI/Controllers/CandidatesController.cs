525c595ea31e9c15e95e30575ddac8a99fb9da50
Bhuwan Subedi <bsubedi@xylontech.com>
2024-10-26 12:43:51 +0545

    Mapper, DI Extension, DbContext  and controller update

--- JobCandidateHubAPI/JobCandidateHubAPI/Controllers/CandidatesController.cs
+++ JobCandidateHubAPI/JobCandidateHubAPI/Controllers/CandidatesController.cs
@@ -10,6 +10,29 @@ namespace JobCandidateHubAPI.Controllers
     [ApiController]
     public class CandidatesController : ControllerBase
     {
-       
+        private readonly ICandidateService _candidateService;
+        private readonly AppDbContext _context;
+        public CandidatesController(ICandidateService candidateService, AppDbContext context)
+        {
+            _candidateService = candidateService;
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpsertCandidate([FromBody] CandidateDto candidateDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _candidateService.UpsertCandidate(candidateDto);
+
+            return Ok(result);
+        }
+        [HttpGet("test")]
+        public IActionResult TestDatabaseConnection()
+        {
+            var count = _context.Candidates.Count();
+            return Ok(new { Count = count });
+        }
     }
 }
