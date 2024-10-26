f3c9349e88a8320c2a048623212c538a3a41a284
Bhuwan Subedi <bsubedi@xylontech.com>
2024-10-26 10:33:36 +0545

    #initial commit project architecture setup

---
+++ JobCandidateHubAPI/JobCandidateHubAPI/Controllers/CandidatesController.cs
@@ -0,0 +1,15 @@
+﻿using JobCandidateHub.Application.DTO;
+using JobCandidateHub.Application.Interfaces;
+using JobCandidateHub.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobCandidateHubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+       
+    }
+}
